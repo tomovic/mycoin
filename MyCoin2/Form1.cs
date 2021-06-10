dd4163ffc16141774780cf1026e29c4b45bf39e7
tomovic <reg_weg@yahoo.de>
2021-06-10 14:39:02 +0200

    tht first step

---
+++ MyCoin2/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.Json;
+using Echovoice.JSON;
+
+namespace MyCoin2
+{
+    public partial class Form1 : Form
+    {
+
+        string[] config_array_type = new string[20];
+        string[] config_array_key = new string[20];
+
+        string[] coin_name_check = new string[40];
+        public Form1()
+        {
+            InitializeComponent();        
+        }
+        private void Load_config_file()
+        {
+            int conuter_for_config_file = 0;
+            var fileName = "c:/mycoin/coingecko-client-config2.json";
+            byte[] data = File.ReadAllBytes(fileName);
+            Utf8JsonReader reader = new Utf8JsonReader(data);
+
+            Console.Write(reader.Read());
+
+            while (reader.Read())
+            {
+                switch (reader.TokenType)
+                {
+
+                    case JsonTokenType.StartObject:
+                        break;
+
+                    case JsonTokenType.PropertyName:
+                        Console.Write($"{reader.GetString()}: ");
+                        config_array_type[conuter_for_config_file] = reader.GetString();
+                        break;
+
+                    case JsonTokenType.String:
+                        Console.WriteLine(reader.GetString());
+                        config_array_key[conuter_for_config_file] = reader.GetString();
+                        conuter_for_config_file++;
+                        break;
+
+                    case JsonTokenType.EndObject:                        
+                        break;
+
+                }
+
+            }
+        }
+
+        private void Load_coin_check_file()
+        {
+            int conuter_for_array = 0;
+            var fileName = "c:/mycoin/coingecko-check_coin.json";
+            byte[] data = File.ReadAllBytes(fileName);
+            Utf8JsonReader reader = new Utf8JsonReader(data);
+            Console.Write(reader.Read());
+
+            while (reader.Read())
+            {
+
+                switch (reader.TokenType)
+                {
+                    case JsonTokenType.StartObject:
+                        break;                   
+
+                    case JsonTokenType.String:
+                        Console.WriteLine(reader.GetString());
+                        coin_name_check[conuter_for_array] = reader.GetString();
+                        break;
+
+                    case JsonTokenType.EndObject:
+                        conuter_for_array++;
+                        break;
+                }
+
+            }
+        }
+      
+        private async void start_Click(object sender, EventArgs e)
+        {
+            Load_config_file();
+            Load_coin_check_file();
+
+            Loadlist mylist = new Loadlist();
+            mylist.address_for_coin_list = "https://api.coingecko.com/api/v3/coins/list?include_platform=true";
+            string result = await mylist.LoaderAsync(); 
+            Console.WriteLine(result); 
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {           
+        }
+    }
+}
---
+++ MyCoin2/Loadlist.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Echovoice.JSON;
+
+namespace MyCoin2
+{
+    internal class Loadlist
+    {
+        public String address_for_coin_list;
+        public String result_from_coinlist_Server;
+
+        public string[] coin_list_id_array = new string[8000];
+        public string[] coin_list_name_array = new string[8000];
+        public async Task<string> LoaderAsync()
+        {
+           
+            using (var httpClient = new HttpClient())
+                result_from_coinlist_Server = await httpClient.GetStringAsync(address_for_coin_list);
+            //  Console.Write(result);
+
+            string[] coint_list_json = JSONDecoders.DecodeJsStringArray(result_from_coinlist_Server);
+
+            string coin_id;
+            string coin_name;
+
+            int counter_for_coin_list_arry = 0;
+
+            for (int a = 0; a < 30000; a++)
+            {
+
+                if (coint_list_json[a][0].ToString() == "{")
+                {
+                    coin_id = coint_list_json[a].Remove(coint_list_json[a].Length - 1, 1);
+                    coin_id = coin_id.Remove(0, 7);
+                    coin_list_id_array[counter_for_coin_list_arry] = coin_id;
+                    Console.WriteLine(coin_id);
+
+                    a = a + 2;
+                    coin_name = coint_list_json[a].Remove(0, 7);
+                    coin_list_name_array[counter_for_coin_list_arry] = coin_name;
+                    Console.WriteLine(coin_name);
+
+                    counter_for_coin_list_arry++;
+                }
+            }
+            return result_from_coinlist_Server;
+        } 
+    }
+
+    
+}
