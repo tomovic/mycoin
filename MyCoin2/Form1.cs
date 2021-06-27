5e61f85b4d9bf7708cc7b0967621f421f916ff86
tomovic <reg_weg@yahoo.de>
2021-06-27 20:41:11 +0200

    With API

--- MyCoin2/Form1.cs
+++ MyCoin2/Form1.cs
@@ -3,14 +3,16 @@ using System.Windows.Forms;
 using System.IO;
 using System.Text.Json;
 using Echovoice.JSON;
+using System.Threading.Tasks;
 
 namespace MyCoin2
 {
     public partial class Form1 : Form
     {
 
-        string[] config_array_type = new string[20];
-        string[] config_array_key = new string[20];
+        string[,] config_array_type = new string[20,20];
+        string[,] config_array_key = new string[20,20];
+    
 
         string[] coin_name_check = new string[40];
         public Form1()
@@ -19,8 +21,10 @@ namespace MyCoin2
         }
         private void Load_config_file()
         {
-            int conuter_for_config_file = 0;
-            var fileName = "c:/mycoin/coingecko-client-config2.json";
+            int counter_for_config_file_key_and_name = 0;
+            int conuter_for_config_file_block = 0;
+            //   var fileName = "c:/mycoin/coingecko-client-config3.json";
+            var fileName = "coingecko-client-config3.json";
             byte[] data = File.ReadAllBytes(fileName);
             Utf8JsonReader reader = new Utf8JsonReader(data);
 
@@ -36,72 +40,94 @@ namespace MyCoin2
 
                     case JsonTokenType.PropertyName:
                         Console.Write($"{reader.GetString()}: ");
-                        config_array_type[conuter_for_config_file] = reader.GetString();
+                        config_array_type[conuter_for_config_file_block, counter_for_config_file_key_and_name] = reader.GetString();
                         break;
 
                     case JsonTokenType.String:
                         Console.WriteLine(reader.GetString());
-                        config_array_key[conuter_for_config_file] = reader.GetString();
-                        conuter_for_config_file++;
-                        break;
-
-                    case JsonTokenType.EndObject:                        
-                        break;
-
-                }
-
-            }
-        }
-
-        private void Load_coin_check_file()
-        {
-            int conuter_for_array = 0;
-            var fileName = "c:/mycoin/coingecko-check_coin.json";
-            byte[] data = File.ReadAllBytes(fileName);
-            Utf8JsonReader reader = new Utf8JsonReader(data);
-            Console.Write(reader.Read());
-
-            while (reader.Read())
-            {
-
-                switch (reader.TokenType)
-                {
-                    case JsonTokenType.StartObject:
-                        break;                   
-
-                    case JsonTokenType.String:
-                        Console.WriteLine(reader.GetString());
-                        coin_name_check[conuter_for_array] = reader.GetString();
+                        config_array_key[conuter_for_config_file_block, counter_for_config_file_key_and_name] = reader.GetString();
+                        counter_for_config_file_key_and_name++;
                         break;
 
                     case JsonTokenType.EndObject:
-                        conuter_for_array++;
+                        counter_for_config_file_key_and_name = 0;
+                        conuter_for_config_file_block++;
                         break;
+
+                }               
+            }
+        }
+
+
+
+        private async void start_Click(object sender, EventArgs e)
+        {
+            
+            Load_config_file();
+
+            Symbol_to_ID symbol_to_ID = new Symbol_to_ID();
+          
+            // search symbol coin in file array...
+            for (int file_block = 0; file_block < 3; file_block++)
+            {
+                for (int file_line = 0; file_line < 4; file_line++)
+                {                 
+                    if(config_array_type[file_block, file_line] == "symbol")
+                    {
+                        Console.WriteLine("----> " + config_array_key[file_block, file_line]);
+                        symbol_to_ID.search_ID_form_file[file_block] = config_array_key[file_block, file_line];
+                    }
+                }               
+            }
+
+            // start id search...
+            symbol_to_ID.create_search();
+
+            await Task.Delay(5000);
+
+            ID_to_enquiry new_convert = new ID_to_enquiry();
+
+            // from Symbol_to_ID in ID_to_enquiry 
+            for (int a = 0; a < 3; a++)
+            {
+                Console.WriteLine(symbol_to_ID.suitable_ID_form_api[a]);
+                new_convert.id_search[a] = symbol_to_ID.suitable_ID_form_api[a];
+            }
+
+            // search rest form file
+            for (int file_block = 0; file_block < 3; file_block++)
+            {
+                Console.WriteLine("-------------- Here is the block-------------");
+                for (int file_line = 0; file_line < 4; file_line++)
+                {
+                    {
+                        if (config_array_key[file_block, file_line] == "true")
+                        {
+                            Console.WriteLine("is true: " + config_array_type[file_block, file_line]);
+                            new_convert.search_form_file[file_block, file_line] = config_array_type[file_block, file_line];
+                        }
+                    }
                 }
 
             }
-        }
-      
-        private async void start_Click(object sender, EventArgs e)
-        {
-            Load_config_file();
-            Load_coin_check_file();
 
-            Loadlist mylist = new Loadlist();
-            mylist.address_for_coin_list = "https://api.coingecko.com/api/v3/coins/list?include_platform=true";
-            string result = await mylist.LoaderAsync(); 
-            Console.WriteLine(result); 
+            new_convert.create_search();
+            
+         
         }
 
         
 
         private void button1_Click(object sender, EventArgs e)
         {
+    
             
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+
         }
 
         private void button3_Click(object sender, EventArgs e)
--- MyCoin2/Loadlist.cs
+++ MyCoin2/Loadlist.cs
@@ -5,45 +5,152 @@ using Echovoice.JSON;
 
 namespace MyCoin2
 {
-    internal class Loadlist
+    internal class ID_to_enquiry
     {
-        public String address_for_coin_list;
-        public String result_from_coinlist_Server;
+        public string[] id_search = new string[10];
+        public string[,] search_return = new string[3,25];
+        public string[,] search_form_file = new string[3, 25];
+        public string[,] master_result = new string[3, 4];
 
-        public string[] coin_list_id_array = new string[8000];
-        public string[] coin_list_name_array = new string[8000];
-        public async Task<string> LoaderAsync()
+        public async void create_search()
         {
+            Geko_API myvari = new Geko_API();         
+
+            for (int a = 0; a < 3; a++)
+            {
+                await Task.Delay(1000);
+                myvari.address_for_coin_list = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=" + id_search[a] + "&order=market_cap_descmarket_cap_desc%2C%20gecko_desc%2C%20gecko_asc%2C%20market_cap_asc%2C%20market_cap_desc%2C%20volume_asc%2C%20volume_desc%2C%20id_asc%2C%20id_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1h";
+
+                string result = await myvari.LoaderAsync();
+
+                string[] coin_list_json = JSONDecoders.DecodeJsStringArray(result);
+
+                for (int b = 0; b < 25; b++)
+                {
+                    search_return[a, b] = coin_list_json[b];
+                }
+
+                Console.WriteLine("--"+ a.ToString() +"----"+ id_search[a] + "--");
+                Console.WriteLine(myvari.address_for_coin_list); 
+                Console.WriteLine(result);
+            }
+
+            // search element form file
+            Console.WriteLine("---form file--");
+            Console.WriteLine(search_form_file[1, 1]);
+            Console.WriteLine("---form file-ende-");
+
            
-            using (var httpClient = new HttpClient())
-                result_from_coinlist_Server = await httpClient.GetStringAsync(address_for_coin_list);
-            //  Console.Write(result);
+            for(int file_block = 0; file_block < 3; file_block++)
+            {
+                Console.WriteLine("----------------");
+                Console.WriteLine("-x-> id: " + id_search[file_block]);
+                for (int coinline_form_web = 0; coinline_form_web < 25; coinline_form_web++)     
+                {
+                
+                    for (int search_line_form_file = 0; search_line_form_file < 4; search_line_form_file++)
+                    {
+                        string[] content_for_savefile = search_return[file_block, coinline_form_web].Split(new Char[] { ':' });
 
-            string[] coint_list_json = JSONDecoders.DecodeJsStringArray(result_from_coinlist_Server);
+                        if (content_for_savefile[0].Remove(content_for_savefile[0].Length - 1, 1) == search_form_file[file_block, search_line_form_file])
+                        {
+                            
+                            Console.WriteLine("--x-->" + content_for_savefile[0] + " : " + content_for_savefile[1]);
+                        }
+                    }
+                    
+                }
+            }
 
+
+        }
+
+    }
+
+    internal class Symbol_to_ID
+    {
+       
+        public string[] search_ID_form_file = new string[10];
+        public string[] suitable_ID_form_api = new string[10];        
+
+        public async void create_search()
+        {
+            
+            Geko_API mylist = new Geko_API();
+            mylist.address_for_coin_list = "https://api.coingecko.com/api/v3/coins/list?include_platform=true";
+            string result = await mylist.LoaderAsync();
+
+            
+            string[] coint_list_json = JSONDecoders.DecodeJsStringArray(result);
             string coin_id;
-            string coin_name;
+            string[,] coin_list_array = new string[9000, 8];
 
-            int counter_for_coin_list_arry = 0;
+        int counter_for_coin_list_arry = 0;
 
             for (int a = 0; a < 30000; a++)
             {
 
                 if (coint_list_json[a][0].ToString() == "{")
                 {
-                    coin_id = coint_list_json[a].Remove(coint_list_json[a].Length - 1, 1);
-                    coin_id = coin_id.Remove(0, 7);
-                    coin_list_id_array[counter_for_coin_list_arry] = coin_id;
-                    Console.WriteLine(coin_id);
 
-                    a = a + 2;
-                    coin_name = coint_list_json[a].Remove(0, 7);
-                    coin_list_name_array[counter_for_coin_list_arry] = coin_name;
-                    Console.WriteLine(coin_name);
+                    if (coint_list_json[a].Substring(coint_list_json[a].Length - 1) == "}")
+                    {
+                        coin_id = coint_list_json[a].Remove(0, 2);
+                        coin_list_array[counter_for_coin_list_arry, 0] = coin_id;   // char 2 with }
+                        Console.WriteLine(1 + " " + coin_id + " end");  // char with }
+                    }
+                    else
+                    {
+                        coin_id = coint_list_json[a].Remove(0, 2);
+                        coin_id = coin_id.Substring(0, coin_id.Length - 1);
+                        coin_list_array[counter_for_coin_list_arry, 0] = coin_id;   // char 2 no }
+                        Console.WriteLine(1 + " " + coin_id);  // char with no  }
 
+                        for (int b = 1; b < 7; b++)
+                        {
+                            if (coint_list_json[a + b].Substring(coint_list_json[a + b].Length - 1) == "}") // CHECK 2 char -> }
+                            {
+                                coin_list_array[counter_for_coin_list_arry, b] = coint_list_json[a + b];   // char 2 no }
+                                Console.WriteLine(1 + b + " " + coint_list_json[a + b] + " end");  // char with }
+                                break;
+                            }
+                            else
+                            {
+                                coin_list_array[counter_for_coin_list_arry, b] = coint_list_json[a + b];   // char 2 no }
+                                Console.WriteLine(1 + b + " " + coint_list_json[a + b]);            // char no }
+                            }
+                        }
+                    }
                     counter_for_coin_list_arry++;
                 }
-            }
+            }        
+
+            for (int a = 0; a < 3; a++)
+            {
+                for (int b = 0; b < 5000; b++)
+                {             
+                       if (coin_list_array[b, 1].Remove(0, 9) == search_ID_form_file[a])
+                       {
+                         Console.WriteLine("------------found-------------");                   
+                         Console.WriteLine(coin_list_array[b, 0].Remove(0, 5));
+                         suitable_ID_form_api[a] = coin_list_array[b, 0].Remove(0, 5);
+                    }
+                }
+            }          
+        }    
+    }
+
+    internal class Geko_API
+    {
+        public String address_for_coin_list;
+        public String result_from_coinlist_Server;
+
+        
+        public async Task<string> LoaderAsync()
+        {
+           
+            using (var httpClient = new HttpClient())
+            result_from_coinlist_Server = await httpClient.GetStringAsync(address_for_coin_list);           
             return result_from_coinlist_Server;
         } 
     }
